5b4ea409eb25295032362aafd5149a51d8ed8702
Indraditya <indraditya@ariadnemaps.com>
2023-02-12 22:58:48 +0100

    Deleted folder and cleaned

--- Tutorial/server/models/BIM.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const ClientSchem = new mongoose.Schema({
-  Buildings: {
-    type: String,
-  },
-});
-
-module.exports = mongoose.model('Client', ClientSchem);
